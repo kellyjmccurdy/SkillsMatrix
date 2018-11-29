5a20a8d2dcc3137ad2aae112f8711f50ae3ed9aa
Kelly <kelly.j.mccurdy@gmail.com>
2018-11-29 13:57:11 -0500

    Add project files.

---
+++ SkillsMatrix.WebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SkillsMatrix.WebMVC.MvcApplication" Language="C#" %>
---
+++ SkillsMatrix.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SkillsMatrix.WebMVC.Startup))]
+namespace SkillsMatrix.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
